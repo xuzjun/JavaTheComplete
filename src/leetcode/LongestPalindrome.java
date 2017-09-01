c01da4662a4d5cc3391a781c82c6739d70d21a90
xuzhojun <xuzhojun@163.com>
2017-09-01 16:56:24 +0800

    409. Longest Palindrome

---
+++ src/leetcode/LongestPalindrome.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+/*
+ * Given a string which consists of lowercase or uppercase letters, 
+ * find the length of the longest palindromes that can be built with those letters.
+ * 
+ * This is case sensitive, for example "Aa" is not considered a palindrome here.
+ * 
+ * Note:
+ * Assume the length of given string will not exceed 1,010.
+ * 
+ * Example:
+ * 
+ * Input:
+ * "abccccdd"
+ * 
+ * Output:
+ * 7
+ * 
+ * Explanation:
+ * One longest palindrome that can be built is "dccaccd", whose length is 7.
+ */
+
+public class LongestPalindrome {
+	
+	class SuffixTree {
+		
+		SuffixTree[] st;
+		
+		boolean isEnd;
+		
+		public SuffixTree() {
+			st = new SuffixTree[52];
+		}
+	}
+	
+	SuffixTree root = new SuffixTree();
+	
+    public int longestPalindrome(String s) {
+    	
+    	for (int i = 0; i < s.length(); ++i) {
+    		String subString = s.substring(i);
+    		
+    		addToSuffixTree(root, subString);
+    	}
+    	
+		return 0;
+    }
+
+	private void addToSuffixTree(SuffixTree root, String subString) {
+		
+		SuffixTree st = root;
+		for (char ch : subString.toCharArray()) {
+		}
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
